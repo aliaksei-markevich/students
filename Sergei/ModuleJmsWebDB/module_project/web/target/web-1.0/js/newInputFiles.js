fb6a089707ace5e89534fe8cc9f4fd4c5951ec24
Pilsik <pilsik1991@gmail.com>
2016-05-25 13:13:06 +0300

    logging web (slf4j+logback)

--- Sergei/ModuleJmsWebDB/module_project/web/target/web-1.0/js/newInputFiles.js
+++
@@ -1,8 +0,0 @@
-;function addInputFile() {
-    var $container = $("div#template", 'form').first().clone();
-    $container.css("marginTop","10px");
-    $container.attr('id','');
-    $container.find("input[type='button']").click(uploadFiles);
-    $container.appendTo('form');
-    $container.show();
-}
--- Sergei/ModuleJmsWebDB/module_project/web/target/web-1.0/views/ErrorJSP.jsp
+++
@@ -1,6 +0,0 @@
-<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-
-<p>Данной страницы не существует</p>
-Error page
